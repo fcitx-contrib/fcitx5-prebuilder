3cfe2b816974a201f7d87e43bad3e62e0aa1af81
Qijia Liu <liumeo@pku.edu.cn>
2024-12-28 12:56:56 -0500

    js

--- scripts/libmozc.py
+++ scripts/libmozc.py
@@ -1,13 +1,35 @@
-from common import CMakeBuilder, INSTALL_PREFIX, ensure
+import platform
+import subprocess
+from common import CMakeBuilder, INSTALL_PREFIX, PLATFORM, ROOT, ensure
+
+no_addon = '-DBUILD_MOZC_ADDON=OFF'
+
+if PLATFORM == 'js':
+    native = {
+        'Linux': 'linux',
+        'Darwin': 'macos'
+    }[platform.system()] + '-' + platform.machine()
+    build_dir = f'libmozc/build/{native}'
+    ensure('cmake', [
+        '-S', 'libmozc', '-B', build_dir,
+        '-G', 'Ninja', '-DCMAKE_BUILD_TYPE=Release',
+        no_addon
+    ])
+    ensure('cmake', [
+        '--build', build_dir,
+        '--target', 'protoc'
+    ])
+    protoc_exe = f'{ROOT}/{build_dir}/protoc'
+
 
 class MozcBuilder(CMakeBuilder):
     def build(self):
         ensure('cmake', ['--build', self.build_, '--target', 'absl_flags_parse'])
+        ensure('cmake', ['--build', self.build_, '--target', 'libprotobuf'])
         ensure('cmake', ['--build', self.build_, '--target', 'mozc-static'])
 
     def install(self):
         usr_dir = f'{self.dest_dir}{INSTALL_PREFIX}'
-        bin_dir = f'{usr_dir}/bin'
         mozc_include_dir = f'{usr_dir}/include/mozc'
         dictionary_include_dir = f'{mozc_include_dir}/dictionary'
         protocol_include_dir = f'{mozc_include_dir}/protocol'
@@ -15,13 +37,17 @@ class MozcBuilder(CMakeBuilder):
         lib_dir = f'{usr_dir}/lib'
         ensure('mkdir', [
             '-p',
-            bin_dir,
             dictionary_include_dir,
             protocol_include_dir,
             usage_stats_include_dir,
             lib_dir
         ])
-        ensure('cp', [f'{self.build_}/protoc', bin_dir]) # iOS needs it.
+
+        if PLATFORM == 'macos':
+            bin_dir = f'{usr_dir}/bin'
+            ensure('mkdir', ['-p', bin_dir])
+            ensure('cp', [f'{self.build_}/protoc', bin_dir]) # iOS needs it.
+
         ensure('cp', [f'{self.build_}/dictionary/pos_matcher_impl.inc', dictionary_include_dir])
         ensure('cp', [f'{self.build_}/protocol/*.pb.h', protocol_include_dir])
         ensure('cp', [f'{self.build_}/usage_stats/usage_stats.pb.h', usage_stats_include_dir])
@@ -31,11 +57,25 @@ class MozcBuilder(CMakeBuilder):
             f'{self.build_}/libmozc-static.a',
             lib_dir
         ])
-        ensure('libtool', [
-            '-o', f'{lib_dir}/libabsl.a',
-            f'$(find {self.build_}/mozc/src/third_party/abseil-cpp -name "*.a")'
-        ])
 
-MozcBuilder('libmozc', [
-    '-DBUILD_MOZC_ADDON=OFF'
-]).exec()
+        libabsl_a = f'{lib_dir}/libabsl.a'
+        all_libabsl_a = f'find {self.build_}/mozc/src/third_party/abseil-cpp -name "*.a"'
+        if native.startswith('linux'):
+            libs = subprocess.check_output(all_libabsl_a, shell=True, text=True).strip().split('\n')
+            absl_script = f'{self.build_}/absl_script'
+            with open(absl_script, 'w') as f:
+                f.write(f'''create {libabsl_a}
+{'\n'.join(f'addlib {lib}' for lib in libs)}
+save
+end
+''')
+            ensure('ar', ['-M', f'< {absl_script}'])
+        else:
+            ensure('libtool', ['-o', libabsl_a, f'$({all_libabsl_a})'])
+
+
+options = [no_addon]
+if PLATFORM == 'js':
+    options.append(f'-DPROTOC_EXECUTABLE={protoc_exe}')
+
+MozcBuilder('libmozc', options).exec()

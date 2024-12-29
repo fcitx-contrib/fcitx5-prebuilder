0f40aec45fe7bded73213d7ad2b4294a950e4c6c
Qijia Liu <liumeo@pku.edu.cn>
2024-12-29 13:59:13 -0500

    clean up

--- scripts/libmozc.py
+++ scripts/libmozc.py
@@ -1,5 +1,10 @@
+# macos -> macos: package protoc
+# macos -> ios: steal protoc
+# macos -> js: steal protoc
+# linux -> js: build protoc first
+
 import platform
-from common import CMakeBuilder, IOS_PLATFORM, INSTALL_PREFIX, PLATFORM, ROOT, ensure, patch, steal
+from common import CMakeBuilder, INSTALL_PREFIX, PLATFORM, ROOT, ensure, patch, steal
 
 no_addon = '-DBUILD_MOZC_ADDON=OFF'
 protoc_exe = ''
@@ -8,50 +13,19 @@ options = [no_addon]
 
 class MozcBuilder(CMakeBuilder):
     def install(self):
-        if PLATFORM == 'ios':
-            layer = 'Release-iphone' + ('simulator' if IOS_PLATFORM == 'SIMULATOR' else 'os') + '/'
-        else:
-            layer = ''
-
-        usr_dir = f'{self.dest_dir}{INSTALL_PREFIX}'
-        mozc_include_dir = f'{usr_dir}/include/mozc'
-        dictionary_include_dir = f'{mozc_include_dir}/dictionary'
-        protocol_include_dir = f'{mozc_include_dir}/protocol'
-        usage_stats_include_dir = f'{mozc_include_dir}/usage_stats'
-        lib_dir = f'{usr_dir}/lib'
-        ensure('mkdir', [
-            '-p',
-            dictionary_include_dir,
-            protocol_include_dir,
-            usage_stats_include_dir,
-            lib_dir
-        ])
-
-        if PLATFORM == 'macos':
-            bin_dir = f'{usr_dir}/bin'
-            ensure('mkdir', ['-p', bin_dir])
-            ensure('cp', [f'{self.build_}/protoc', bin_dir]) # iOS needs it.
-
-        ensure('cp', [f'{self.build_}/dictionary/pos_matcher_impl.inc', dictionary_include_dir])
-        ensure('cp', [f'{self.build_}/protocol/*.pb.h', protocol_include_dir])
-        ensure('cp', [f'{self.build_}/usage_stats/usage_stats.pb.h', usage_stats_include_dir])
-        ensure('cp', [
-            f'{self.build_}/mozc/src/third_party/protobuf/third_party/utf8_range/{layer}libutf8_validity.a',
-            f'{self.build_}/mozc/src/third_party/protobuf/{layer}libprotobuf.a',
-            f'{self.build_}/{layer}libmozc-static.a',
-            lib_dir
-        ])
-
+        super().install()
+        # Combine all .o files of absl to libabsl.a
+        lib_dir = f'{self.dest_dir}{INSTALL_PREFIX}/lib'
         libabsl_a = f'{lib_dir}/libabsl.a'
-        if PLATFORM == 'ios':
-            all_libabsl_a = f'$(find {self.build_} -name "*.o" | grep build/absl)'
-        else:
-            all_libabsl_a = f'$(find {self.build_}/mozc/src/third_party/abseil-cpp -name "*.o")'
-        ensure('ar', ['rc', libabsl_a, all_libabsl_a])
+        if PLATFORM == 'ios': # Xcode
+            all_libabsl_o = f'$(find {self.build_} -name "*.o" | grep build/absl)'
+        else: # Ninja
+            all_libabsl_o = f'$(find {self.build_}/mozc/src/third_party/abseil-cpp -name "*.o")'
+        ensure('ar', ['rc', libabsl_a, all_libabsl_o])
 
     def pre_package(self):
         if PLATFORM != 'macos':
-            ensure('rm', ['-rf', f'{self.dest_dir}/usr/bin'])
+            ensure('rm', ['-rf', f'{self.dest_dir}{INSTALL_PREFIX}/bin'])
 
 
 if PLATFORM == 'js':
@@ -71,7 +45,7 @@ if PLATFORM == 'js':
 
 if platform.system() == 'Darwin' and PLATFORM != 'macos':
     steal('libmozc', ('bin',)) # extracted to install dir so need to remove on prepack.
-    protoc_exe = f'{MozcBuilder('libmozc').dest_dir}/usr/bin/protoc'
+    protoc_exe = f'{MozcBuilder('libmozc').dest_dir}{INSTALL_PREFIX}/bin/protoc'
 
 if PLATFORM in ('ios', 'js'):
     options.append(f'-DPROTOC_EXECUTABLE={protoc_exe}')

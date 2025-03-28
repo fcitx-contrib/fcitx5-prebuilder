7cd655c4e38d10b678a68232767fb809f935da0a
Qijia Liu <liumeo@pku.edu.cn>
2025-03-28 07:32:52 -0400

    bundle librime-qjs (#23)

--- scripts/librime.py
+++ scripts/librime.py
@@ -1,19 +1,30 @@
 import os
 
-from common import CMakeBuilder, ROOT, USR, ensure, patch
+from common import CMakeBuilder, INSTALL_PREFIX, ROOT, USR, ensure, patch
 
 project = 'librime'
 
 patch(project)
 patch('librime-lua')
+patch('librime-qjs')
 
 os.chdir('librime/plugins')
-for plugin in ('lua', 'octagram', 'predict'):
+for plugin in ('lua', 'octagram', 'predict', 'qjs'):
     if not os.path.islink(plugin):
         ensure('ln', ['-s', f'../../librime-{plugin}', plugin])
 os.chdir(ROOT)
 
-CMakeBuilder(project, [
+
+class LibrimeBuilder(CMakeBuilder):
+    def install(self):
+        super().install()
+        ensure('cp', [
+            f'{ROOT}/librime-qjs/build/libqjs.a',
+            f'{self.dest_dir}{INSTALL_PREFIX}/lib'
+        ])
+
+
+LibrimeBuilder(project, [
     '-DBUILD_TOOLS=OFF', # propagate to plugins
     '-DBUILD_TEST=OFF',
     '-DBUILD_MERGED_PLUGINS=ON',

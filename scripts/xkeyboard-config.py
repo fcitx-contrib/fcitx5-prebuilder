8cdedbca62d7c8e42c0d17de86ba0ae478b7c9ba
Qijia Liu <liumeo@pku.edu.cn>
2025-08-21 17:38:12 -0400

    deduplicate xkb mo

--- scripts/xkeyboard-config.py
+++ scripts/xkeyboard-config.py
@@ -4,5 +4,7 @@ class XKeyboardConfigBuilder(MesonBuilder):
     def pre_package(self):
         # Remove X11 to avoid unpack symlink failure on Windows (f5h).
         ensure('rm', ['-rf', f'{self.dest_dir}/usr/share/X11'])
+        # Remove xkeyboard-config-2.mo which is just a duplicate and not registered by fcitx5.
+        ensure('find', [self.dest_dir, '-name', 'xkeyboard-config-2.mo', '-delete'])
 
 XKeyboardConfigBuilder('xkeyboard-config').exec()

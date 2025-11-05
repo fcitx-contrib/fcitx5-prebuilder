a1f1b1e3ba5b2daa23a72a80f03fa9778a8fa9f0
Qijia Liu <liumeo@pku.edu.cn>
2025-11-04 20:42:25 -0500

    rust libchewing !windows !ios !harmony (#52)

--- scripts/libchewing.py
+++ scripts/libchewing.py
@@ -1,4 +1,4 @@
-from common import CMakeBuilder, PLATFORM, patch, steal
+from common import CMakeBuilder, INSTALL_PREFIX, MACOS_ARCH, PLATFORM, ensure, patch, steal
 
 project = 'libchewing'
 
@@ -7,8 +7,25 @@ patch(project)
 if PLATFORM != 'macos':
     steal(project)
 
-CMakeBuilder(project, [
+cargo_target = ''
+if PLATFORM == 'macos':
+    cargo_target = f'{MACOS_ARCH.replace('arm64', 'aarch64')}-apple-darwin'
+elif PLATFORM == 'js':
+    cargo_target = 'wasm32-unknown-emscripten'
+
+
+class ChewingBuilder(CMakeBuilder):
+    def install(self):
+        super().install()
+        # libchewing.a only has an empty .c, which is only useful for building .so.
+        ensure('cp', [
+            f'{self.build_}/libchewing_capi.a',
+            f'{self.dest_dir}{INSTALL_PREFIX}/lib/libchewing.a'
+        ])
+
+
+ChewingBuilder(project, [
     '-DWITH_SQLITE3=OFF',
     '-DBUILD_TESTING=OFF',
-    '-DWITH_RUST=OFF'
+    f'-DRust_CARGO_TARGET={cargo_target}'
 ]).exec()

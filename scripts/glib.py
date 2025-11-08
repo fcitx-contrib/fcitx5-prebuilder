97ce7e35400a6428bc1de459098d1a5eba7ca263
Qijia Liu <liumeo@pku.edu.cn>
2025-11-08 17:28:21 -0500

    js: glib !windows !ios !harmony (#54)

--- scripts/glib.py
+++ scripts/glib.py
@@ -1,9 +1,18 @@
-from common import MesonBuilder, patch
+import os
+from common import PLATFORM, ROOT, MesonBuilder, ensure, patch
 
+version = '2.87.0'
 project = 'glib'
 
 # Make sure glib selects proxy-libintl
-patch(project)
+if PLATFORM == 'js':
+    os.chdir('glib')
+    branch = f'fcitx-{version}'
+    ensure('git', ['fetch', 'https://github.com/fcitx-contrib/glib', f'{branch}:{branch}'])
+    ensure('git', ['checkout', branch])
+    os.chdir(ROOT)
+else:
+    patch(project)
 
 MesonBuilder(project, [
     '-Dtests=false',

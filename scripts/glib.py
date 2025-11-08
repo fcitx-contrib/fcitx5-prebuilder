9bd4b07d840237f7330d7196479ad0e800a7e762
Qijia Liu <liumeo@pku.edu.cn>
2025-11-07 23:52:19 -0500

    no history and we don't want to pull history

--- scripts/glib.py
+++ scripts/glib.py
@@ -1,13 +1,12 @@
 import os
-import subprocess
 from common import PLATFORM, ROOT, MesonBuilder, ensure, patch
 
+version = '2.87.0'
 project = 'glib'
 
 # Make sure glib selects proxy-libintl
 if PLATFORM == 'js':
     os.chdir('glib')
-    version = subprocess.check_output("git describe", shell=True, text=True).strip()
     branch = f'fcitx-{version}'
     ensure('git', ['fetch', 'https://github.com/fcitx-contrib/glib', f'{branch}:{branch}'])
     ensure('git', ['checkout', branch])

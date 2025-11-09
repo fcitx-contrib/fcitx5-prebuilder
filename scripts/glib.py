d7bfa205e39a16cd5ead6c8ade0da2ada524f08b
Qijia Liu <liumeo@pku.edu.cn>
2025-11-09 07:28:45 -0500

    preserve bin as referenced in .pc

--- scripts/glib.py
+++ scripts/glib.py
@@ -21,7 +21,6 @@ class GLibBuilder(MesonBuilder):
             bak = f'{file}.bak'
             ensure('sed', ['-i.bak', '"s|-pthread||g"', file])
             ensure('rm', [bak])
-        ensure('rm', ['-rf', f'{self.dest_dir}{INSTALL_PREFIX}/bin'])
 
 GLibBuilder(project, [
     '-Dtests=false',

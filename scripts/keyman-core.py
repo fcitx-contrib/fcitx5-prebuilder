8a9a30d3c026509886d6e3aa6fda781765674462
Qijia Liu <liumeo@pku.edu.cn>
2025-12-05 18:50:12 -0500

    manual procedure

--- scripts/keyman-core.py
+++ scripts/keyman-core.py
@@ -1,7 +1,10 @@
-from common import MesonBuilder, patch
+from common import MesonBuilder, ensure, patch
 
 project = 'keyman-core'
 patch(project)
+
+ensure('mkdir', ['-p', 'keyman-core/resources'])
+ensure('cp', ['keyman/resources/build/meson/standard.meson.build', 'keyman-core/resources/meson.build'])
 
 MesonBuilder(project, [
     '-Ddebug=false', # Reproducible: absolute path

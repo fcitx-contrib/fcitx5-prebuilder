a561c787bdcf9dace0cfaffc4b27ddbb0d2af26d
Qijia Liu <liumeo@pku.edu.cn>
2025-12-05 17:52:20 -0500

    drop json-c [skip ci]

--- scripts/json-c.py
+++
@@ -1,8 +0,0 @@
-from common import CMakeBuilder
-
-CMakeBuilder('json-c', [
-    '-DDISABLE_WERROR=YES', # ios: https://github.com/json-c/json-c/issues/808
-    '-DBUILD_TESTING=OFF',
-    '-DBUILD_APPS=OFF',
-    '-DDISABLE_EXTRA_LIBS=ON'
-]).exec()
